42645bbf3853f415f51c0ed3cf76992045586896
cimerr <88597268+Grigory776@users.noreply.github.com>
2021-08-11 23:19:37 +0800

    Add files via upload

---
+++ !ФреймворкС++.cpp
@@ -0,0 +1,79 @@
+﻿// !ФреймворкС++.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include <sstream>
+#include <exception>
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
+
+template <class T>
+ostream& operator << (ostream& os, const set<T>& s) { // вывод контейнера сет(множество)
+    os << "{";
+    bool first = true;
+    for (const auto& x : s) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << x;
+    }
+    return os << "}";
+}
+
+template <class K, class V>
+ostream& operator << (ostream& os, const map<K, V>& m) { // вывод контейнера мэп(словарь)
+    os << "{";
+    bool first = true;
+    for (const auto& kv : m) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << kv.first << ": " << kv.second;
+    }
+    return os << "}";
+}
+
+template<class T, class U> // шаблонная функция сравнения
+void AssertEqual(const T& t, const U& u,
+    const string& hint)
+{
+    if (t != u) {
+        ostringstream os;
+        os << "Assertion failed: " << t << " != " << u
+            << " hint: " << hint;
+        throw runtime_error(os.str());
+    }
+}
+
+inline void Assert(bool b, const string& hint) { /**/
+    AssertEqual(b, true, hint);
+}
+
+class TestRunner { // класс который ловит исключения, в метод объекта(ран тест) данного класа, следует передавать Тесты на ошибки с ииспользование ассерт эколь
+public:
+    template <class TestFunc>
+    void RunTest(TestFunc func, const string& test_name) {
+        try {
+            func();
+            cerr << test_name << " OK" << endl;
+        }
+        catch (runtime_error& e) {
+            ++fail_count;
+            cerr << test_name << " fail: " << e.what() << endl;
+        }
+    }
+
+    ~TestRunner() {
+        if (fail_count > 0) {
+            cerr << fail_count << " unit tests failed. Terminate" << endl;
+            exit(1);
+        }
+    }
+
+private:
+    int fail_count = 0;
+};
+
